aaf0c58822a7d13fc1cfe883be5d53cf72c7bed7
JYChoi07 <jin8907@gmail.com>
2023-02-03 11:30:07 +0900

    first streamlit app

---
+++ first_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+
+def main():
+    st.title("First_APP")
+    df = pd.DataFrame({
+        'first_col':[1, 2, 3, 4],
+        'second_col':[10, 20, 30, 40]
+    })
+    st.write(df)
+
+if __name__ == "__main__":
+    main()
